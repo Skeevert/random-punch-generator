45d60f951c3adc146178864a8b1dd301e331e76b
Skeevert <45735359+Skeevert@users.noreply.github.com>
2019-06-26 16:10:46 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -21,99 +20,62 @@ static unsigned int		ft_line_count(int fd)
 	return (line_count + 1);
 }
 
-static char				*ft_rand_str(int fd)
+void	ft_rand_str(int fd)
 {
 	unsigned int	i;
 	unsigned int	line;
-	char			*str;
+	char			ch;
 	size_t			offset;
-	size_t			str_size;
 
+	lseek(fd, 0, SEEK_SET);
 	i = 0;
 	srand(time(NULL));
 	line = rand() % (ft_line_count(fd) - 1);
-	str = (char *)malloc(1);
-	*str = '#';
-	str_size = 0;
+	ch = '#';
 	offset = 0;
 	while (line > 0)
 	{
-		read(fd, str, 1);
-		if (*str == '\n')
+		read(fd, &ch, 1);
+		if (ch == '\n')
 			line--;
-		offset++;
 	}
-	*str = '#';
-	while (*str != '\n')
-	{
-		read(fd, str, 1);
-		str_size++;
-	}
-	free(str);
-	if (!(str = (char *)malloc(sizeof(*str) * (str_size + 1))))
-		return (NULL);
-	lseek(fd, offset, SEEK_SET);
-	while (str_size > 1)
-	{
-		read(fd, (str + i), 1);
-		i++;
-		str_size--;
-	}
-	*(str + i) = '\0';
-	lseek(fd, 0, SEEK_SET);
-	return (str);
 }
 
-static char				*ft_generate(int setup_fd, int punch_fd)
+void	ft_print_punch(int punch_fd)
 {
-	char	*setup;
-	char	*punch;
-	char	*output;
-	char	*punch_start;
-	char	*setup_start;
-	int		i;
-	int		size;
-
-	i = 0;
-	setup = ft_rand_str(setup_fd);
-	setup_start = setup;
-	size = strlen(setup) + 1;
-	output = (char *)malloc(sizeof(*output) * size);
-	while (*setup)
+	char	ch;
+	
+	ch = ' ';
+	ft_rand_str(punch_fd);
+	while(ch != '\n' && ch)
 	{
-		if (*setup == '#')
-		{
-			sleep(1);
-			punch = ft_rand_str(punch_fd);
-			punch_start = punch;
-			size = size + strlen(punch);
-			if(!(output = (char *)realloc(output, sizeof(*output) * size)))
-				return (NULL);
-			while (*punch)
-			{
-				output[i] = *punch;
-				i++;
-				punch = punch + 1;
-			}
-			free(punch_start);
-		}
-		else
-		{
-			output[i] = *setup;
-			i++;
-		}
-		setup = setup + 1;
+		read(punch_fd, &ch, 1);
+		write(1, &ch, 1);
 	}
-	output[i] = '\0';
-	free(setup_start);
-	return (output);
 }
 
-int						main(void)
+void	ft_print_setup(int setup_fd, int punch_fd)
+{
+	char	ch;
+	
+	ch = ' ';
+	ft_rand_str(setup_fd);
+	while(ch != '\n' && ch)
+	{
+		read(setup_fd, &ch, 1);
+		if (ch == '#')
+		{
+			ft_print_punch(punch_fd);
+			read(setup_fd, &ch, 1);
+		}
+		write(1, &ch, 1);
+	}
+}
+
+int		main(void)
 {
 	int		setup_fd;
 	int		punch_fd;
-	char	*output;
 
 	if ((setup_fd = open("setup", O_RDONLY)) == -1)
 		{
@@ -125,13 +87,6 @@ int						main(void)
 			write(2, "Cannot find file \"punch\"\n", 25);
 			return (1);
 		}
-	output = ft_generate(setup_fd, punch_fd);
-	if (!output)
-		write(2, "Oops!\n", 6);
-	else
-		write(1, output, strlen(output));
-	write(1, "\n", 1);
-	close(setup_fd);
-	close(punch_fd);
+	ft_print_setup(setup_fd, punch_fd);
 	return (0);
 }
